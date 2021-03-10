9e9d160364ca192a45cf834fbdb0eecfa42c986a
Yimika Olotin <holayinkajr@gmail.com>
2021-03-11 00:01:07 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const image = document.getElementById('intro-img');
+const button = document.getElementById('intro-btn');
+
+window.addEventListener('scroll', function() {
+    button.style.marginBottom = '1em';
+});
