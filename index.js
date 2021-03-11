3d402da3d8a5019680a194792b4d9a4305522083
Yimika Olotin <holayinkajr@gmail.com>
2021-03-11 19:39:10 +0100

    worked on about and testimonial sections

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const image = document.getElementById('intro-img');
-const button = document.getElementById('intro-btn');
+const iconbuttons = document.getElementById('intro-icons');
 
 window.addEventListener('scroll', function() {
-    button.style.marginBottom = '1em';
+    iconbuttons.style.marginBottom = '3em';
 });
